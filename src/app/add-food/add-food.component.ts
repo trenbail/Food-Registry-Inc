b1a41b895f4c68527cf6b104823249eba91966e6
rajkumar-patel <raj.m.patel.16@gmail.com>
2019-10-09 16:23:22 -0500

    more connect stuff

--- src/app/add-food/add-food.component.ts
+++ src/app/add-food/add-food.component.ts
@@ -11,7 +11,10 @@ export class AddFoodComponent implements OnInit {
 
   addFood(nf:NgForm){
     nf.value["members"] = {} 
-    this.hc.post("18.221.215.81:3000/inventory/registerFood",nf.value,{headers:{"Content-Type":"application/json"}});
+    this.hc.post("http://18.221.215.81:3000/inventory/registerFood",nf.value,{headers:{"Content-Type":"application/json"}})
+    .subscribe((res) => {
+      console.log(res)
+    });
 
     console.log("Food OBJECT =>", nf.value)
   }
