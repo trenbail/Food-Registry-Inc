75955ddb27b99a0fcb720b394aa8ea75716ceb87
rajkumar-patel <raj.m.patel.16@gmail.com>
2019-10-10 14:08:50 -0500

    connected components to db

--- src/app/add-food/add-food.component.ts
+++ src/app/add-food/add-food.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
-
+import { APIService } from '../shared/api.service'
 
 import {NgForm} from '@angular/forms'
 @Component({
@@ -10,18 +9,13 @@ import {NgForm} from '@angular/forms'
 export class AddFoodComponent implements OnInit {
 
   addFood(nf:NgForm){
-    nf.value["members"] = {} 
-    this.hc.post("http://18.221.215.81:3000/inventory/registerFood",nf.value,{headers:{"Content-Type":"application/json"}})
-    .subscribe((res) => {
-      console.log(res)
-    });
-
+    this.apiS.post("/inventory/registerFood", nf.value, function(res) {console.log(res)})
     console.log("Food OBJECT =>", nf.value)
   }
 
 
   
-  constructor(private hc: HttpClient) {}
+  constructor(private apiS: APIService) {}
 
   ngOnInit() {
   }
