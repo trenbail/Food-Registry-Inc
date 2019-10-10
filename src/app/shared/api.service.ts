83cfb4e56cabe3b2135f6afe9d8a55814d586eb2
hutcheson8 <hutcheson8@gmail.com>
2019-10-10 09:33:09 -0500

    Updated URL for API

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
-const APIBaseURL = "http://18.221.215.81:3000";
+const APIBaseURL = "http://18.221.215.81/api";
 
 @Injectable()
 export class APIService {
